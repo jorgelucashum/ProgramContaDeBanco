6c0d3360abc524b2912905468738f87ec84ade31
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-17 15:30:22 -0300

    Add project files.

---
+++ ProgramContaDeBanco/ContaBanco.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace ProgramContaDeBanco
+{
+    class ContaBanco
+    {
+        private string _titular;
+        public int NumConta { get; private set; }
+        public double SaldoTotal  { get; private set; }
+
+        public ContaBanco(string titular, int numConta)
+        {
+            _titular = titular;
+            NumConta = numConta;
+            
+        }
+        public ContaBanco(string titular, int numConta, double depositoInicial) : this(titular, numConta)
+        {
+            Depositar(depositoInicial);
+           
+        }
+
+        public string GetTitular()
+        {
+            return _titular;
+        }
+
+        public void SetTitular(string titular)
+        {
+            if (titular != null && titular.Length > 1) { 
+            _titular = titular;                        }
+        }
+
+        public void Depositar(double val)
+        {
+            SaldoTotal +=  val;
+        }
+
+        public void Sacar(double val)
+        {
+            SaldoTotal -= val + 5;
+        }
+
+        public override string ToString()
+        {
+            return "\nDados da conta:\nConta: " + NumConta + ", Titular: " + _titular + ", Saldo: R$" + SaldoTotal.ToString("F2", CultureInfo.InvariantCulture) + "\n";
+        }
+
+    }
+}
---
+++ ProgramContaDeBanco/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramContaDeBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBanco conta;
+
+            Console.Write("Digite o número da conta do banco: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titular: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial? (s/n): ");
+            char x = char.Parse(Console.ReadLine());
+            if (x == 's' || x == 'S')
+            {
+                Console.Write("Digite o valor do depósito: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBanco(titular, numero, depositoInicial);
+                
+            } else
+            {
+                conta = new ContaBanco(titular, numero);
+            }
+
+            Console.WriteLine(conta);
+
+            Console.Write("Digite um valor para depositar: ");
+            conta.Depositar(double.Parse(Console.ReadLine()));
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Digite um valor para sacar: ");
+            conta.Sacar(double.Parse(Console.ReadLine()));
+            Console.WriteLine(conta);
+
+            Console.ReadLine();
+        }
+    }
+}
